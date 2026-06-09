3dcdbd4d2b7671d086f67867035b0a78d011fc6a
One4Lots <sahorkot.ontime@gmail.com>
2026-06-09 17:17:40 +0600

    fs/pstore/ram.c: fixed array type 'char[36]' is not assignable

--- fs/pstore/ram.c
+++ fs/pstore/ram.c
@@ -371,7 +371,7 @@ static size_t ramoops_write_kmsg_hdr(struct persistent_ram_zone *prz,
 	char hdr[36]; /* "===="(4), %lld(20), "."(1), %06lu(6), "-%c\n"(3) */
 	size_t len;
 
-	hdr = kasprintf(GFP_ATOMIC, RAMOOPS_KERNMSG_HDR TVSEC_FMT ".%lu-%c\n",
+	len = scnprintf(hdr, sizeof(hdr),RAMOOPS_KERNMSG_HDR "%lu.%lu-%c\n",
 		record->time.tv_sec,
 		record->time.tv_nsec / 1000,
 		record->compressed ? 'C' : 'D');
